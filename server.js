3d0d1ef3325ff90968f3d5dbfbbd19f8a244c432
Simon Aquilina <aquilina_simon@hotmail.com>
2024-03-24 18:50:59 +0100

    test

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/message", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+app.listen(port, () => {
+  console.log('Server is running on port ' + port);
+});
+
+
